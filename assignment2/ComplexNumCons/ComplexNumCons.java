09e926d309d142b4b3e64d053ef657b2a7ef0f20
chandrakant100 <chandrakant.barcelona@gmail.com>
2020-07-31 08:41:55 +0530

    fixed the scanner issue

--- assignment2/ComplexNumCons/ComplexNumCons.java
+++ assignment2/ComplexNumCons/ComplexNumCons.java
@@ -4,20 +4,20 @@ class ComplexNum {
     float real;
     float img;
 
-    ComplexNum() {
-        Scanner sc = new Scanner(System.in);
+    ComplexNum(Scanner sc) {
+        
 
         System.out.print("Enter Real number: ");
         real = sc.nextFloat();
         System.out.print("Enter Imaginary number: ");
         img = sc.nextFloat();
+    
     }
-
     void output() {
         System.out.println("Complex number: " + real + " + " + img + "i");
     }
 
-    void add(ComplexNum var1, ComplexNum var2) {
+    ComplexNum(ComplexNum var1, ComplexNum var2) {
         real = var1.real + var2.real;
         img  = var1.img + var2.img;
     }
@@ -25,15 +25,17 @@ class ComplexNum {
 
 public class ComplexNumCons {
     public static void main(String[] args) {
-        ComplexNum Obj1 = new ComplexNum();
+
+        Scanner sc = new Scanner(System.in);
+        ComplexNum Obj1 = new ComplexNum(sc);
         Obj1.output();
 
-        ComplexNum Obj2 = new ComplexNum();
+        ComplexNum Obj2 = new ComplexNum(sc);
         Obj2.output();
 
         System.out.println("\tADDITION OF TWO COMPLEX NUMBERS");
-        ComplexNum Obj3 = new ComplexNum();
-        Obj3.add(Obj1, Obj2);
+        ComplexNum Obj3 = new ComplexNum(Obj1, Obj2);
         Obj3.output();
+        sc.close();
     }
 }
